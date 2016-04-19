5a2ddd550d5e999df8fc5fee75c330cca8f59975
Lars Michaelis <mail@larmic.de>
2016-04-19 18:44:38 +0200

    add omnifaces push socket example

--- src/main/java/de/larmic/butterfaces/omnifaces/WebSocketBean.java
+++ src/main/java/de/larmic/butterfaces/omnifaces/WebSocketBean.java
@@ -1,7 +1,11 @@
 package de.larmic.butterfaces.omnifaces;
 
 
+import org.omnifaces.cdi.Push;
+import org.omnifaces.cdi.PushContext;
+
 import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
 
@@ -9,8 +13,19 @@ import java.io.Serializable;
 @ViewScoped
 public class WebSocketBean implements Serializable {
 
-    public String getTestValue() {
-        return "hello world!";
+    private String message;
+
+    @Inject
+    @Push
+    private PushContext someChannel;
+
+    public void setMessage(String message) {
+        this.message = message;
+        someChannel.send(message);
+    }
+
+    public String getMessage() {
+        return message;
     }
 
 }
